faf728a06a652d9d1b491324554ec57fd62deb93
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2022-01-30 16:41:08 +0100

    - refactor include to follow best practice - split the CMakeLists.txt

---
+++ include/fizzbuzz/base.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+std::vector<std::string> fizzbuzz(uint32_t n);
---
+++ src/fizzbuzz.cpp
@@ -0,0 +1,7 @@
+#include <fizzbuzz/base.h>
+
+std::vector<std::string> fizzbuzz(uint32_t n) {
+    std::vector<std::string> result;
+
+    return result;
+}
